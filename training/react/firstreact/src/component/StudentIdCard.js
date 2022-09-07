6c2ea3ea58e0f0691132e9f0a87d13dac94357c7
nensi <ndnensidarji@gmail.com>
2022-09-07 19:04:28 +0530

    update

--- training/react/firstreact/src/component/StudentIdCard.js
+++ training/react/firstreact/src/component/StudentIdCard.js
@@ -1,14 +1,14 @@
 import Personal from "./Personal";
 
 const StudentIdCard = () =>
-    <section class="admin-dashboard">
-        <div class="container">
-            <div class="row mb-3">
-                <div class="col-lg-12 lg:ps-5 pt-3 column10">
+    <section className="admin-dashboard">
+        <div className="container">
+            <div className="row mb-3">
+                <div className="col-lg-12 lg:ps-5 pt-3 column10">
                     {/* <!-- Data Add Part - Form --> */}
-                    <div class="p-5 bg-white shadow-lg bg-opacity-75 rounded fw-bolder border border-2 border-danger adminServiceForm wow fadeInUp" data-wow-delay="0.1s">
-                        <h3 class="text-center text-decoration-underline mb-5 text-danger">Student Information</h3>
-                        <form method="" enctype="multipart/form-data" id="addServiceForm">
+                    <div className="p-5 bg-white shadow-lg bg-opacity-75 rounded fw-bolder border border-2 border-danger adminServiceForm wow fadeInUp" data-wow-delay="0.1s">
+                        <h3 className="text-center text-decoration-underline mb-5 text-danger">Student Information</h3>
+                        <form method="">
                             <Personal />
                         </form>
                     </div>
